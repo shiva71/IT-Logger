b3c6737994f19d8971a29d1c580005ac9c70c5b4
shiva <shivashandilya1@gmail.com>
2020-07-03 16:41:51 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+
+const parth = require('path');
 //Init Middleware
 
 app.use(express.json({ extended: false }));
@@ -7,6 +9,15 @@ app.use(express.json({ extended: false }));
 //Define Route
 app.use('/api/logs', require('./routes/logs'));
 app.use('/api/techs', require('./routes/techs'));
+
+//Server Static assest in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  appu.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
